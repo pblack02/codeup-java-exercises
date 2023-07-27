4c0846d5316decb50c32e0cd16c3acc63f037db0
Presley Black <presleyblack24@gmail.com>
2023-07-27 15:00:52 -0500

    Hello world exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        how we 'log' to the console
+        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
+    }
+}
