6cf24f724b8677f44c3aba076481de8dafcd27bd
Presley Black <presleyblack24@gmail.com>
2023-08-04 11:03:48 -0500

    update exercise

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -7,9 +7,9 @@ public class CircleApp {
         Circle myCircle = new Circle();
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a radius in meters");
-        double userimput = scan.nextDouble();
-        myCircle.radius = userimput;
-        System.out.printf("Your radius is %f%n", userimput);
+        double userNum = scan.nextDouble();
+        myCircle.radius = userNum;
+        System.out.printf("Your radius is %f%n", userNum);
         System.out.printf("Your area is %f%n",myCircle.getArea());
         System.out.printf("Your Circumference is %f", myCircle.getCircumference());
 
